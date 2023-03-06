b248feee4b584d17a1d71d6e6124b8ab3e1b4a75
Daniel Haehn <haehn@mpsych.org>
2023-03-06 17:12:12 +0000

    From last time.

--- calculator/controller/mathengine.py
+++ calculator/controller/mathengine.py
@@ -7,5 +7,9 @@ class MathEngine:
     print('123 Welcome to the Math Engine')
     pass
 
+  def add(self, n1, n2):
+
+    return n1+n2
+
 
 
--- calculator/view/ui.py
+++ calculator/view/ui.py
@@ -3,4 +3,13 @@ class UI:
   def __init__(self):
   	pass
 
-  
+  def run(self, controller):
+
+    print('Please enter 2 numbers')
+
+    n1 = float(input('Number 1 ?'))
+
+    n2 = float(input('Number 2 ?'))
+    
+    return controller.add(n1, n2)
+     
